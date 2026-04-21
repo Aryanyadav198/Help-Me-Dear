67e9f421b59673ef78e3e9fd0be56694c2b7da94
Aryan yadav <aryanyadav898487@gmail.com>
2026-04-21 13:09:07 +0600

    fix firebase env config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-// import dotenv from "dotenv";
-// dotenv.config({ path: "./.env" });
 
-// import connectDb from "./db/index.js";
-// import { app } from "./app.js";
-// // 
-
-// connectDb()
-//   .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//       console.log(`✅ Server running on PORT: ${process.env.PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("❌ DB connection failed. Server not started.", err);
-//     process.exit(1);
-//   });
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
